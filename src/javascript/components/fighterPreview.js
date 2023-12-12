a1846d926a46376ba334a8af7f315954620a242b
vlk2022 <volodymyrkostiuk2021@gmail.com>
2023-12-13 00:33:03 +0200

    + created info fighter

--- src/javascript/components/fighterPreview.js
+++ src/javascript/components/fighterPreview.js
@@ -7,7 +7,31 @@ export function createFighterPreview(fighter, position) {
         className: `fighter-preview___root ${positionClassName}`
     });
 
-    // todo: show fighter info (image, name, health, etc.)
+    if (fighter) {
+        const imageFighter = createElement({
+            tagName: 'img',
+            attributes: {
+                src: fighter.source,
+                height: '200px'
+            }
+        });
+
+        const fighterInfoElement = createElement({
+            tagName: 'div',
+            className: 'fighter-preview___info'
+        });
+
+        const { name, attack, defence, health } = fighter;
+        fighterInfoElement.innerHTML = `
+            <p>Name: ${name}</p>
+            <p>Attack: ${attack}</p>
+            <p>Defence: ${defence}</p>
+            <p>Health: ${health}</p>
+        `;
+
+        fighterElement.appendChild(imageFighter);
+        fighterElement.appendChild(fighterInfoElement);
+    }
 
     return fighterElement;
 }
